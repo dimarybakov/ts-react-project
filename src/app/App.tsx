63e6c808844a97bff0ebec0b42faeb3b85c8dce6
Dmitriy Rybakov <dk.rybakov@gmail.com>
2024-08-26 16:34:36 +0300

    i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,14 +8,22 @@ import { AboutPage } from 'pages/AboutPage';
 import { MainPage } from 'pages/MainPage';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
+import { useTranslation } from 'react-i18next';
 
 const App = () => {
   const { theme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Navbar />
-      <AppRouter />
+      <Suspense fallback="">
+        <Navbar />
+
+        <div className="content-page">
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   );
 };
