4620a9d8769f93393a47a488bc6b9fc7d0e6ff0d
Dmitriy Rybakov <dk.rybakov@gmail.com>
2024-09-19 12:46:57 +0300

    Install redux, move ErrorBoundary to providers

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
-import { ErrorBoundary } from 'app/ErrorBoundary';
+import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import App from './app/App';
 import 'app/styles/index.scss';
 
