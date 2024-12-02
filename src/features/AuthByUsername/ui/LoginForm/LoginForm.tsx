86d19088aaf7d1834d5948c8c7ea894ff1629a4f
Dmitriy Rybakov <dk.rybakov@gmail.com>
2024-12-02 14:47:37 +0300

    Add basic loginModal

---
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -0,0 +1,22 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button/Button';
+import cls from './LoginForm.module.scss';
+
+interface LoginFormProps {
+  className?: string;
+}
+
+export const LoginForm: FC<LoginFormProps> = (props) => {
+  const { className } = props;
+  const { t } = useTranslation();
+
+  return (
+    <div className={classNames(cls.loginForm, {}, [className])}>
+      <input type="text" />
+      <input type="text" />
+      <Button>{t('Войти')}</Button>
+    </div>
+  );
+};
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -4,6 +4,7 @@ import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { LoginModal } from 'features/AuthByUsername';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -14,8 +15,12 @@ export const Navbar = ({ className }: NavbarProps) => {
   const { t } = useTranslation();
   const [isAuthModal, setIsAuthModal] = useState(false);
 
-  const onToggleModal = useCallback(() => {
-    setIsAuthModal((prev) => !prev);
+  const onCloseModal = useCallback(() => {
+    setIsAuthModal(false);
+  }, []);
+
+  const onShowModal = useCallback(() => {
+    setIsAuthModal(true);
   }, []);
 
   return (
@@ -23,17 +28,11 @@ export const Navbar = ({ className }: NavbarProps) => {
       <Button
         theme={ButtonTheme.CLEAR_INVERTED}
         className={cls.links}
-        onClick={onToggleModal}
+        onClick={onShowModal}
       >
         {t('Войти')}
       </Button>
-      <Modal isOpen={isAuthModal} onClose={() => setIsAuthModal(false)}>
-        {/* eslint-disable i18next/no-literal-string */}
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur aut
-        soluta at et obcaecati culpa perspiciatis molestiae eos. Perspiciatis
-        delectus hic mollitia at, laudantium temporibus quo! Architecto nulla
-        dicta nostrum?
-      </Modal>
+      <LoginModal isOpen={isAuthModal} onClose={onCloseModal} />
     </div>
   );
 };
