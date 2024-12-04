6f298aa09bac293329f3471a5188914b6ed1b501
Dmitriy Rybakov <dk.rybakov@gmail.com>
2024-12-04 15:03:14 +0300

    add pre commit hooks, fix screenshots

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,7 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { FC } from 'react';
-import cls from './LoginModal.module.scss';
 import { LoginForm } from '../LoginForm/LoginForm';
 
 interface LoginModalProps {
@@ -15,7 +14,7 @@ export const LoginModal: FC<LoginModalProps> = (props) => {
 
   return (
     <Modal
-      className={classNames(cls.loginModal, {}, [className])}
+      className={classNames('', {}, [className])}
       isOpen={isOpen}
       onClose={onClose}
       lazy
